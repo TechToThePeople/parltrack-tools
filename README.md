955cb10c1e04d34020c0746c5106b605fcce9ecf
Xavier Dutoit <xavier@tttp.eu>
2014-02-02 19:57:49 +0100

    add more fields to the export

--- README.md
+++ README.md
@@ -3,9 +3,15 @@ As requested at the hackathon, small scripts I have lying around parltrack
 Standing on giants shoulders.
 
 json2csv.php
+-------------
 
 Extract some info from ep_current_meps.json dump and creates a csv from it
 
 See a problem or want more columns? Pull Requests welcome and all that jazz.
 
 Love data, put them in good hands and go out campaining
+
+csvtool
+----------
+
+ csvtool col 1,2,3,5-7 ep_meps_current.csv >
--- json2csv.php
+++ json2csv.php
@@ -4,26 +4,31 @@ $file = "ep_meps_current";
 $fp = fopen($file.'.csv', 'w');
 
 $meps= json_decode (file_get_contents($file.'.json'));
-$out=array ('epid','first_name','last_name','email','birthdate','gender','eugroup','phone','office','committee','delegation');
+$out=array ('epid','country','first_name','last_name','email','birthdate','gender','eugroup','phone','office','committee','delegation');
 
 foreach ($meps as $mep){
 //if (!$mep->Constituencies[0]->end) {
 if (!$mep->active) {
-  echo 'skip '. $mep->Name->full;
+  echo "\nskip ". $mep->Name->full;
   continue;
 }
 //print_r($mep);
 $out = array (
   $mep->UserID,
+  $mep->Constituencies[0]->country,
   $mep->Name->sur,
   $mep->Name->family,
   $mep->Mail[0],
   substr($mep->Birth->date,0,10),
   $mep->Gender,
-  $mep->Groups[0]->groupid,
-  $mep->Addresses->Brussels->Phone,
-  $mep->Addresses->Brussels->Address->Office,
-);
+  $mep->Groups[0]->groupid);
+  if (isset($mep->Addresses) && isset ($mep->Addresses->Brussels)) {
+    $out[] = $mep->Addresses->Brussels->Phone;
+    $out[] = $mep->Addresses->Brussels->Address->Office;
+  } else {
+   $out[]= null;
+   $out[]= null;
+  }
   if (is_array($out[6])) 
     $out[6] = implode('/',$out[6]);
 if (isset($mep->Committees)) {

e0d564e46b776ec92058e74d7fe5fc5c5d9b485a
xavier <xavier@tttp.eu>
2015-03-10 23:25:16 +0100

    update Tue Mar 10 23:25:15 CET 2015

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 curl http://parltrack.euwiki.org/dumps/ep_meps_current.json.xz | xz -cd > ep_meps_current.json
- fgrep '"active": true' ep_meps_current.json |  jq 'del(.changes)' > ep_mep_active.json
+cat ep_meps_current.json | fgrep '"active": true' |  jq -c 'del(.changes)' | { echo '['; sed 's/$/,/'; echo '{}]'; }  > ep_mep_active.json 
+
+#cat ep_meps_current.json | sed '1s/^\[//; n;d;' > ep_meps_separated.json
+#cat ep_meps_current.json  | fgrep '"active": true' | sed 's/}$/},/' > tmp.json
+#sed -i '1i[' tmp.json
+#jq 'del(.changes)' > ep_mep_active.json
 #nice -n19 jq "map(select(.active) | del(.changes))" ep_meps_current.json > ep_mep_active.json
 nice -n19 php json2csv.php
 git commit -am "update `date`"
